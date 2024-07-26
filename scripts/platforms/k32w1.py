3222ccb82b16054fa7f3d0fc3115a3c17c064615
marius-alex-tache <marius.tache@nxp.com>
2024-07-26 11:29:19 +0300

    [k32w1] Update metadata to be 1.4.0-pvw1 compliant
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- scripts/platforms/k32w1.py
+++ scripts/platforms/k32w1.py
@@ -16,35 +16,25 @@
 #
 import os
 
-from .platform import Platform
+from .utils.platform import Platform
+from .utils.tools import BlHost
 
-from spsdk.utils import nxpdevscan
-
-# This assumes that the repo is a submodule inside the Matter repository.
-# Otherwise, the relative path will not work.
-github_sdk = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../github_sdk/rw_k32w1/repo'))
+# This assumes that the matter-vscode-for-mcux repo is cloned in Matter root path.
+github_sdk = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../third_party/nxp/nxp_matter_support/github_sdk/common_sdk/repo'))
+nbu_path = f"{github_sdk}/middleware/wireless/ieee-802.15.4/bin/k32w1/k32w1_nbu_ble_15_4_dyn_matter_1_0_17_1.sb3"
 
 class K32W1(Platform):
 
     def __init__(self):
         super().__init__()
 
-        self.tool = ["blhost", "-p"]
-        self.actions.append(["receive-sb-file", "metadata/k32w1/binaries/example-nbu.sb3"])
-        self.actions.append(["flash-erase-region", "0xf4000", "8192"])
-        self.actions.append(["write-memory", "0xf4000", "metadata/k32w1/binaries/example-factory-data.bin", "8192"])
-
-    def run(self):
-        uart_device = None
-        nxp_uart_devices = nxpdevscan.search_nxp_uart_devices()
-        if nxp_uart_devices:
-            uart_device = nxp_uart_devices[0].name
-        self.run_actions(uart_device)
+        self.tool = BlHost()
+        self.tool.add_action(["receive-sb-file", nbu_path])
+        self.tool.add_action(["flash-erase-region", "0xf4000", "8192"])
+        self.tool.add_action(["write-memory", "0xf4000", "metadata/k32w1/binaries/example-factory-data.bin", "8192"])
+        self.tool.add_action(["reset"])
 
     def pre_message(self):
         print("Please place the board in ISP mode:")
         print(" - place jumper on JP25")
         print(" - press and hold SW4, press and release Reset, then release SW4")
-
-    def post_message(self):
-        print("Board is ready for application flashing")
