d58faa3bb0d29cd32d669977d37b69dabf22a12c
marius-alex-tache <marius.tache@nxp.com>
2024-07-26 11:29:57 +0300

    [rw61x] Add initial support
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- scripts/flash_utils.py
+++ scripts/flash_utils.py
@@ -19,10 +19,12 @@ import click
 
 from platforms.k32w0 import K32W0
 from platforms.k32w1 import K32W1
+from platforms.rw61x import RW61X
 
 PLATFORMS = {
     "k32w0": K32W0,
-    "k32w1": K32W1
+    "k32w1": K32W1,
+    "rw61x": RW61X
 }
 
 @click.command()
